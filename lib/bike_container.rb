b760ce6be0114cb3cc4456b3939e4faba5c10fc0
samover <samuel.overloop@gmail.com>
2015-11-15 23:13:51 +0000

    module mixins: add tests for adding bikes

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -1,14 +1,28 @@
 module BikeContainer
+  attr_reader :capacity
+
   DEFAULT_CAPACITY = 20
 
   def initialize(capacity: DEFAULT_CAPACITY)
-    @docker = capacity
+    @capacity = capacity
+    @container = []
   end
 
-  def capacity
-    docker
+  def bikes
+    container
   end
+
+  def add bike
+    fail "Cannot dock bike: #{self.class.name} is full to capacity" if full?
+    container << bike
+  end
+
 
   private
-  attr_reader :docker
+  attr_reader :docker, :container
+
+  def full?
+    bikes.length >= capacity
+  end
+
 end
--- spec/support/shared_examples_for_bike_container.rb
+++ spec/support/shared_examples_for_bike_container.rb
@@ -1,5 +1,31 @@
 shared_examples_for BikeContainer do
-  it 'has a default capacity when initialized' do
-    expect(subject.capacity).to eq BikeContainer::DEFAULT_CAPACITY
+
+  subject(:bike_container) { described_class.new }
+  let(:bike) { double(:bike) }
+
+  context '#initialize' do
+    it 'has a default capacity when initialized' do
+      expect(bike_container.capacity).to eq BikeContainer::DEFAULT_CAPACITY
+    end
+
+    it 'can have another capacity when created' do
+      random_capacity = rand(100)
+      bike_container = described_class.new(capacity: random_capacity)
+      expect(bike_container.capacity).to eq random_capacity
+    end
   end
+
+  context '#add' do
+    it 'adds a bike to the container' do
+      expect { bike_container.add bike }.not_to raise_error
+    end
+
+    it 'raises error when container is full' do
+      bike_container.capacity.times { bike_container.add bike }
+      message = "Cannot dock bike: #{self.class.name} is full to capacity"
+      expect { bike_container.add bike }.to raise_error
+    end
+  end
+
+
 end

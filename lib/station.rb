403b9bc059cfd0af433a219c12276652c4e688d0
samover <samuel.overloop@gmail.com>
2015-11-15 23:58:40 +0000

    mixin modules: integrate with station class

--- lib/station.rb
+++ lib/station.rb
@@ -3,17 +3,8 @@ require_relative 'bike_container'
 class Station
   include BikeContainer
 
-  # DEF_CAPACITY = 20
-
-  # def initialize(capacity: DEF_CAPACITY)
-  #   @docker = []
-  #   @capacity = capacity
-  # end
-
   def dock bike
     add bike
-    # fail 'Cannot dock bike: station is full to capacity' if full?
-    # docker << bike
   end
 
   def release_bike
@@ -27,30 +18,7 @@ class Station
     released
   end
 
-  # def bikes
-  #   docker
-  # end
-  #
-  # def remove_broken bikes
-  #   self.docker -= bikes
-  # end
-
-  # def read_capacity
-  #   capacity
-  # end
-
   private
-  # attr_reader :capacity
-  # attr_accessor :docker
-
-  # def empty?
-  #   working_bikes.empty?
-  # end
-
-  # def full?
-  #   bikes.length >= capacity
-  # end
-
   def working_bikes
     bikes.select { |bike| bike.working? }
   end
